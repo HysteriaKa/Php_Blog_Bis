008a90803982d6cf43bc15f2414c2aca427f4fcc
HysteriaKa <karine2310@gmail.com>
2021-10-05 14:48:39 +0200

    codacy & fix notif

--- src/controllers/ErrorHandler.php
+++ src/controllers/ErrorHandler.php
@@ -5,9 +5,9 @@ class ErrorHandler
     public function __construct($err, $msg=null) {
         // var_dump($err);
         global $currentSession;
-        $currentSession->addNotification("error",  $msg !== null ? $msg : "le serveur a rencontré une erreur :(");
-        $currentSession->addNotification("success",  $msg !== null ? $msg : "le serveur a rencontré une erreur :(");
-        $currentSession->addNotification("warn",  $msg !== null ? $msg : "le serveur a rencontré une erreur :(");
+        $currentSession->addNotification("error",  $msg !== null ? $msg : "le serveur a rencontré une erreur :(<br>$err");
+        //$currentSession->addNotification("success",  $msg !== null ? $msg : "le serveur a rencontré une erreur :(");
+        //$currentSession->addNotification("warn",  $msg !== null ? $msg : "le serveur a rencontré une erreur :(");
 
     }
 }
